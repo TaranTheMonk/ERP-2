ef451b76ffeeae0b2aec661ee48a12f758e4397e
Xulang Wan <taran.xulang.wan@gmail.com>
2022-07-05 01:47:54 +0800

    update

--- Makefile
+++ Makefile
@@ -3,3 +3,9 @@ create-env:
 
 install-dependencies:
 	pip install -r ./requirements.txt
+
+process-raw-data:
+	python ./scripts/process_raw_data.py
+
+run:
+	python ./src/main.py
---
+++ scripts/process_raw_data.py
@@ -0,0 +1,101 @@
+import os
+import random
+import pandas
+import pathlib
+
+RESOURCE_PATH = os.path.join(pathlib.Path(__file__).parent, "../.resources")
+
+
+def process_worker_data():
+    for raw_worker_file in os.listdir(os.path.join(RESOURCE_PATH, "raw_data/worker")):
+        # read raw data
+        with open(
+            os.path.join(RESOURCE_PATH, "raw_data/worker", raw_worker_file), "r"
+        ) as f:
+            lines = f.readlines()
+
+        # process raw data
+        df = {
+            "lat": [],
+            "lon": [],
+            "min_lat": [],
+            "min_lon": [],
+            "max_lat": [],
+            "max_lon": [],
+            "velocity": [],
+        }
+        for line in lines:
+            # data template
+            # id; lat; lon; capacity; activeness; [min_lat, min_lon, max_lat, max_lon]; reliability; velocity
+            line = line.split(";")
+            df["lat"].append(float(line[1]))
+            df["lon"].append(float(line[2]))
+            df["min_lat"].append(float(line[5][1:]))
+            df["min_lon"].append(float(line[6]))
+            df["max_lat"].append(float(line[7]))
+            df["max_lon"].append(float(line[8][:-1]))
+            df["velocity"].append(float(line[10]))
+
+        # write processed data
+        df = pandas.DataFrame(df)
+        df.to_csv(
+            os.path.join(
+                RESOURCE_PATH,
+                "processed_data/worker",
+                raw_worker_file.split(".")[0] + ".csv",
+            ),
+            index=False,
+        )
+
+
+def process_task_data():
+    for raw_worker_file in os.listdir(os.path.join(RESOURCE_PATH, "raw_data/task")):
+        # read raw data
+        with open(
+            os.path.join(RESOURCE_PATH, "raw_data/task", raw_worker_file), "r"
+        ) as f:
+            lines = f.readlines()
+
+        # process raw data
+        df = {
+            "lat": [],
+            "lon": [],
+            "deadline": [],
+            "workload": [],
+            "expected_time": [],
+            "penalty_rate": [],
+            "reward": [],
+        }
+        for line in lines:
+            # data template
+            # lat; lon; arrival_time; expiry_time; requirement; confidence; entropy
+            line = line.split(";")
+            deadline = float(line[3]) - float(line[2])
+            expected_time = random.uniform(2 / 5 * deadline, 3 / 5 * deadline)
+            reward = random.uniform(0.0, 1.0)
+
+            df["lat"].append(float(line[0]))
+            df["lon"].append(float(line[1]))
+            df["deadline"].append(deadline)
+            df["workload"].append(random.uniform(2 / 5 * deadline, 2 * deadline))
+            df["expected_time"].append(expected_time)
+            df["penalty_rate"].append(
+                random.uniform(0.0, reward / (deadline - expected_time))
+            )
+            df["reward"].append(reward)
+
+        # write processed data
+        df = pandas.DataFrame(df)
+        df.to_csv(
+            os.path.join(
+                RESOURCE_PATH,
+                "processed_data/task",
+                raw_worker_file.split(".")[0] + ".csv",
+            ),
+            index=False,
+        )
+
+
+if __name__ == "__main__":
+    process_worker_data()
+    process_task_data()
